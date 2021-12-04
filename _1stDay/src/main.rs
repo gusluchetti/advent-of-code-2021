11a36f5ed30b90e4b783cf2a36f32c341d17fe7f
Gustavo Luchetti <gusta.luchetti@gmail.com>
2021-12-04 15:37:07 -0300

    created cargo project and added input text file for first day puzzle

--- _1stDay/src/main.rs
+++ _1stDay/src/main.rs
@@ -1,3 +1,20 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path
+};
+
+fn depth_from_input(filename: impl AsRef<Path>) -> Vec<i32> {
+    let file = File::open(filename).expect("File doesn't exist");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lines = depth_from_input("input.txt");
+    for line in lines {
+        println!("{:?}", line);
+    }
 }
---
+++ _1stDay/target/debug/deps/_1stDay-08fbab41966cad39.d
@@ -0,0 +1,5 @@
+/mnt/p/advent-of-code-2021/_1stDay/target/debug/deps/_1stDay-08fbab41966cad39: src/main.rs
+
+/mnt/p/advent-of-code-2021/_1stDay/target/debug/deps/_1stDay-08fbab41966cad39.d: src/main.rs
+
+src/main.rs:
