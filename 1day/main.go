9ae6067b41ece1b769f18b1acf423f1dd459fc3e
Gustavo Luchetti <gusta.luchetti@gmail.com>
2021-12-05 16:38:38 -0300

    finished part 1 of first day puzzle

--- 1day/main.go
+++ 1day/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "os"
+	"fmt"
+	"os"
+	"log"
+	"bufio"
+	"strconv"
 )
 
 func main() {
-	dat, err := os.ReadFile("input.txt")
-	fmt.Print(String(dat))
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	var depthArray []int;
+
+	for scanner.Scan() {
+		depth, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+		}
+		depthArray = append(depthArray, depth)
+	}
+	
+	var counter int = 0;
+	for i:=1; i < len(depthArray); i++ {
+		if depthArray[i] > depthArray[i-1] {
+			fmt.Printf("Depth %d is bigger than depth %d \n", depthArray[i], depthArray[i-1])
+			counter++
+			fmt.Printf("Counter increased! It is now at %d \n", counter)
+		}
+	}
+
+	fmt.Println("Number of times depth increased: " + strconv.Itoa(counter))
 }
