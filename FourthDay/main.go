cc0c77631dbe7ce8f473e7876e8d56da1d3990e8
Gustavo Luchetti <gusta.luchetti@gmail.com>
2021-12-11 19:26:17 -0300

    wip bingo logic and added tests

--- FourthDay/main.go
+++ FourthDay/main.go
@@ -32,9 +32,6 @@ func GetBoardsAndStates(tokens []string) ([][][]int, [][][]bool) {
 	
 	for i := 2; i < len(tokens); i++ {
 		split := strings.Fields(tokens[i])
-		fmt.Printf("\nBoard Number: %d, Line Number: %d", boardNumber, lineNumber)
-		fmt.Printf("\nCurrent split: %s [Length %d]\n", split, len(split))
-		
 		if len(split) == 5 {
 			board[lineNumber] = make([]int, 5)
 			state[lineNumber] = make([]bool, 5)
@@ -49,7 +46,6 @@ func GetBoardsAndStates(tokens []string) ([][][]int, [][][]bool) {
 			fmt.Printf("\nFinished board and state: %v, %v \n", board, state)
 			boards = append(boards, board)
 			states = append(states, state)
-
 			boardNumber++
 			lineNumber = 0
 		}
@@ -58,13 +54,105 @@ func GetBoardsAndStates(tokens []string) ([][][]int, [][][]bool) {
 	return boards, states
 }
 
-func CheckWinConditions() bool {
-	isGameFinished := false
+func CheckBoardState(state [][]bool) bool {
+	victoryAchieved := true
+	for i, array := range state {
+		for j, single := range array {
+			fmt.Println(array)
+			length := len(array) - 1
+			half := length / 2
+			if (single) {
+				// check if win might be diagonal
+				if (i == j && i + j == length) {
+					if ((i == half) || (i < half && j < half) || (i > half && j > half)) { // top left to bot right diag
+						for b := 0; b<=length; b++ {
+							y := 0
+							if (!state[y][b]) {
+								victoryAchieved = false
+								break
+							}
+							y++
+						}
+						if (victoryAchieved) {
+							return true
+						}
+					} else if (i == half){ // bot left to top right diag
+						for b := 0; b<=length; b++ {
+							y := length
+							if (!state[y][b]) {
+								victoryAchieved = false
+								break
+							}
+							y--
+						}
+						if (victoryAchieved) {
+							return true
+						}
+					}
+				}
+				// check for horizontal win
+				for _, n := range state[i] {
+					if (!n) {
+						victoryAchieved = false
+						break
+					}
+				}
+				if (victoryAchieved) {
+					return true
+				}
+				// check for vertical win
+				for _, m := range state {
+					if(!m[j]) {
+						victoryAchieved = false
+						break
+					}
+				}
+				if (victoryAchieved) {
+					return true
+				}
+			}
+		}
+	}
 
-
-	return isGameFinished
+	return victoryAchieved
 }
 
+func GameLoop(drawnNumbers []int, boards [][][]int, states[][][]bool) int {
+	var lastDrawnNumber int
+	var timesFound int
+	gameFinished := false
+	score := 0	
+	
+	color.Blue("\nGame is about to start!")
+	for i, current := range drawnNumbers {
+		color.Cyan("\n#%d Number is %d", i, current)
+		for b, board := range boards {
+			for y, array := range board {
+				for x, number := range array {
+					if number ==	current {
+						states[b][y][x] = true
+						timesFound++
+					}				
+				}
+			}
+			// checking if that board won the game
+			if (CheckBoardState(states[b])) {
+				lastDrawnNumber = current
+				gameFinished = true
+				break
+			}
+		}
+		color.Cyan("That number was found %d times!", timesFound)
+
+		if (gameFinished) {
+			// todo: calculate score correctly
+			score = 10 * lastDrawnNumber
+			break
+		}
+	}
+
+	return score
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -77,14 +165,7 @@ func main() {
 		tokens = append(tokens, scanner.Text())
 	}
 
-	drawnNumbers := GetDrawnNumbers(tokens)
 	boards, states := GetBoardsAndStates(tokens)
-
-	color.Blue("Game is about to start!")
-	// game loop
-	isGameFinished := false
-	for isGameFinished != true {
-
-		isGameFinished = CheckWinConditions()
-	}
+	score := GameLoop(GetDrawnNumbers(tokens), boards, states)
+	fmt.Printf("Score for winning board was %d", score)
 }
