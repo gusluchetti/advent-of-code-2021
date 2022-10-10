c1589e77cd231a191afc1fd5617a31a51b3087f3
Gustavo Luchetti <gustavo@luchetti.dev>
2022-10-10 23:20:27 +0200

    feat: auto get all years inputs

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "github.com/gusluchetti/advent-of-code/utils"
 
 func main() {
-	utils.DumpInput(2019)
+	// getting all inputs so far
+    years := [7]int{2015, 2016, 2017, 2018, 2019, 2020, 2021}
+	for _, y := range years {
+		utils.DumpInput(y)
+	}
 }
