feea9e9128b842ece315ce2b1ac9126915d1d82a
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-18 15:48:26 -0300

    feat: move utils and wip parse input

--- src/utils/parse_input.rs
+++ src/utils/parse_input.rs
@@ -1,15 +1,30 @@
 use std::{io, io::prelude::*};
-use std::vec;
 use std::fs::File;
 
-// can get input in a couple ways:
-// default - no separator (whole input as string)
+// parse puzzle input in a couple ways:
+// default - no separator (whole input as ONE string)
 // new line separator (array of lines)
 // other separator (any other character or word)
-fn parse_input(path: String) -> Result<Vec<String>, io::Error> {
+
+
+fn parse_input(path: String, separator: &str) -> io::Result<()> {
     let mut file = File::open(path)?;
-    let mut input = String::new();
-    file.read_to_string(&mut input)?;
+    let mut input: String = String::from("test");
+
+    match separator {
+        "none" => {
+            let mut input = String::new();
+            file.read_to_string(&mut input)?;
+        },
+        "new_line" => {
+            file.read_to_end(&mut input)?;
+        },
+        _ => {
+            file.read_to_end(&mut input)?;
+        }
+    };
+
+    println!("{:?}", input);
 
     return Ok(input);
 }
