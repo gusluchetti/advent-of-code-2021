2a99a160b784fa0e9a567ef06ae45e5e059718f7
Gustavo Luchetti <gustavo@luchetti.dev>
2023-06-19 14:21:52 -0300

    feat: add 3 separator types to read input

--- src/utils/parse_input.rs
+++ src/utils/parse_input.rs
@@ -1,42 +1,50 @@
 use std::{io, io::prelude::*};
 use std::fs::File;
+use std::str;
 
 // parse puzzle input in a couple ways:
 // default - no separator (whole input as ONE string)
 // new line separator (array of lines)
 // other separator (any other character or word)
 
-pub fn parse_input(path: String, separator: &str) -> io::Result<()> {
-    let mut file = File::open(path)?;
-    let mut lines: Vec<String> = Vec::new();
-    // let new_line: u8 = '\n' as u8;
+enum Separator {
+    None,
+    NewLine,
+    Other
+}
 
-    match separator {
+pub fn parse_input(path: String, separator: &str) -> io::Result<()> {
+    let line_end: u8 = '\n' as u8;
+    let mut file = File::open(path)?;
+    let mut lines: Vec<&str> = Vec::new();
+
+    let mut input = Vec::new();
+    file.read_to_end(&mut input)?;
+
+    let lines = match separator {
         "none" => {
-            let mut input = String::new();
-            file.read_to_string(&mut input)?;
-            println!("{:?}", input);
+            let single = str::from_utf8(&input).unwrap();
+            lines.push(single);
+            lines
         },
         "new_line" => {
-            let mut input = Vec::new();
-            file.read_to_end(&mut input)?;
-            println!("{:?}", input);
-            for (i, b) in input.iter().enumerate() {
-                let mut last_cr: usize = 0;
-                let cr: u8 = '\r' as u8;
-                if &cr == b {
-                    lines.push(input[last_cr..i-1])
-                    println!("last CR at index {last_cr}")
+            let mut last_cr: usize = 0;
+            for (i, byte) in input.iter().enumerate() {
+                if byte == &line_end {
+                    let slice: &[u8] = &input[last_cr+1..i-1];
+                    let line: &str = str::from_utf8(slice).unwrap();
+                    lines.push(line);
                     last_cr = i;
                 }
             }
+            lines
         },
         _ => {
-            let mut input = Vec::new();
             file.read_to_end(&mut input)?;
-            println!("{:?}", input);
+            lines
         }
     };
 
+    println!("{:?}", lines);
     return Ok(());
 }
